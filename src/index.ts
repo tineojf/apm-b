584a06caf37f7c394424549e93af1dc8d52f3c4f
angelgamonal <angelgamonal.t@gmail.com>
2025-05-15 20:50:21 -0500

    feat: add stream route to fetch OpenAI API

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Application } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes";
+import chatRoutes from "./routes/chatRoutes";
 
 dotenv.config();
 
@@ -13,6 +14,8 @@ app.use(cors());
 
 // Routes
 app.use("/api/v1/user", userRoutes);
+
+app.use("/api/v1/chat", chatRoutes);
 
 // Port
 const PORT = Number(process.env.PORT) || 5000;
