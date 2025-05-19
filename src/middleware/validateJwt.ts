8f15d502f5d1b1f6194c4f6f695c4b21e61ae188
angelgamonal <angelgamonal.t@gmail.com>
2025-05-19 10:42:44 -0500

    feat: add JWT authentication middleware

---
+++ src/middleware/validateJwt.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from "express";
+import { supabase } from "../utils/supabaseClient";
+
+export const authenticate = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization?.replace("Bearer ", "");
+
+  if (!token) {
+    res.status(401).json({ error: "No token provided" });
+    return;
+  }
+
+  // Verify token against Supabase
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser(token);
+
+  if (user === null || error) {
+    console.log("error->", error);
+    res.status(401).json({ error: "Token is invalid or expired" });
+    return;
+  }
+
+  console.log("user->", user);
+
+  req["user"] = user;
+  next();
+};
