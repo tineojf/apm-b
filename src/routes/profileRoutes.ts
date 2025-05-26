ffa932e6024e1d171d12c67b8734bf51f4e17ccd
tineojf <92743762+tineodev@users.noreply.github.com>
2025-05-26 16:40:01 -0500

    feat: add authentication middleware to profile routes

--- src/routes/profileRoutes.ts
+++ src/routes/profileRoutes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { getProfile } from "../controllers/profileController";
+import { authenticate } from "../middleware/validateJwt";
 
 const profileRoutes = Router();
+
+profileRoutes.use(authenticate);
 
 profileRoutes.get("/", getProfile);
 
