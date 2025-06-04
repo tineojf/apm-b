9fed73adcbd2327a9dffd01929e148a11e9af4e4
tineojf <92743762+tineodev@users.noreply.github.com>
2025-06-04 14:13:16 -0500

    feat: add updateProfile route to profileRoutes for profile management

--- src/routes/profileRoutes.ts
+++ src/routes/profileRoutes.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { getProfile, createProfile } from "../controllers/profileController";
+import {
+  getProfile,
+  createProfile,
+  updateProfile,
+} from "../controllers/profileController";
 import { authenticate } from "../middleware/validateJwt";
 
 const profileRoutes = Router();
@@ -8,5 +12,6 @@ profileRoutes.use(authenticate);
 
 profileRoutes.get("/", getProfile);
 profileRoutes.post("/", createProfile);
+profileRoutes.put("/", updateProfile);
 
 export default profileRoutes;
