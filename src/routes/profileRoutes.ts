7615ec614b579d7be01b70a8415a3645325d4106
tineojf <92743762+tineodev@users.noreply.github.com>
2025-06-04 16:20:34 -0500

    feat: remove createProfile route and enforce profile validation on updateProfile

--- src/routes/profileRoutes.ts
+++ src/routes/profileRoutes.ts
@@ -1,17 +1,14 @@
 import { Router } from "express";
-import {
-  getProfile,
-  createProfile,
-  updateProfile,
-} from "../controllers/profileController";
+import { getProfile, updateProfile } from "../controllers/profileController";
 import { authenticate } from "../middleware/validateJwt";
+import { validate } from "../middleware/validate";
+import { profileSchema } from "../validators/profile/profileValidator";
 
 const profileRoutes = Router();
 
 profileRoutes.use(authenticate);
 
 profileRoutes.get("/", getProfile);
-profileRoutes.post("/", createProfile);
-profileRoutes.put("/", updateProfile);
+profileRoutes.put("/", validate(profileSchema), updateProfile);
 
 export default profileRoutes;
