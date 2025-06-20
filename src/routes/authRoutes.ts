e828b721e0f38372ec4fd443feef07df71ca41cc
tineojf <92743762+tineodev@users.noreply.github.com>
2025-06-20 13:33:00 -0500

    feat: add middleware for auth routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { loginSchema, registerSchema } from "../validators/auth/authValidator";
+import { validate } from "../middleware/validate";
 import {
   registerUser,
   loginUser,
@@ -8,8 +10,8 @@ import {
 
 const authRoutes = Router();
 
-authRoutes.post("/register", registerUser);
-authRoutes.post("/login", loginUser);
+authRoutes.post("/register", validate(registerSchema), registerUser);
+authRoutes.post("/login", validate(loginSchema), loginUser);
 authRoutes.post("/refresh-token", refreshToken);
 authRoutes.post("/validate-token", validateTokenController);
 
