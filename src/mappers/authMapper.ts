fc34f40121390e1a7552c1c4ad710b9f2e92aa0f
tineojf <92743762+tineodev@users.noreply.github.com>
2025-06-20 16:06:37 -0500

    refactor: update toLoginDTO function to accept fullName parameter

--- src/mappers/authMapper.ts
+++ src/mappers/authMapper.ts
@@ -1,13 +1,5 @@
 import { LoginDTO } from "../models/loginDTO";
 
-export const toLoginDTO = (sbResponse: any, sbResponse2: any): LoginDTO => {
-  return {
-    full_name: sbResponse2.full_name || "User",
-    refresh_token: sbResponse.session.refresh_token || "",
-    access_token: sbResponse.session.access_token || "",
-  };
-};
-
 export const toLoginDTO2 = (sbResponse: any, fullName: string): LoginDTO => {
   return {
     full_name: fullName || "User",
@@ -15,3 +7,43 @@ export const toLoginDTO2 = (sbResponse: any, fullName: string): LoginDTO => {
     access_token: sbResponse.session.access_token || "",
   };
 };
+
+type AuthInput = {
+  email: string;
+  password: string;
+};
+
+type LoginEntity = {
+  session: {
+    refresh_token: string;
+    access_token: string;
+  };
+};
+
+type RegisterEntity = {
+  session: {
+    refresh_token?: string;
+    access_token?: string;
+  };
+};
+
+export function mapToAuthEntity(body: AuthInput) {
+  return {
+    email: body.email,
+    password: body.password,
+  };
+}
+
+export function mapToLoginDTO(authResponse: LoginEntity) {
+  return {
+    refresh_token: authResponse.session.refresh_token || "Not provided",
+    access_token: authResponse.session.access_token || "Not provided",
+  };
+}
+
+export function mapToRegisterDTO(authResponse: RegisterEntity) {
+  return {
+    refresh_token: authResponse.session.refresh_token || "Not provided",
+    access_token: authResponse.session.access_token || "Not provided",
+  };
+}
