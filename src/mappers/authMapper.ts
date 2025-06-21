08f1cc2519c1df3aab9e9b97d6f9767dd6103187
tineojf <92743762+tineodev@users.noreply.github.com>
2025-06-21 10:00:32 -0500

    refactor: remove unused LoginDTO model and related mapping function

--- src/mappers/authMapper.ts
+++ src/mappers/authMapper.ts
@@ -1,14 +1,5 @@
-import { LoginDTO } from "../models/loginDTO";
 import { Profile } from "../types/supabase";
 import { LoginInput } from "../validators/auth/authValidator";
-
-export const toLoginDTO2 = (sbResponse: any, fullName: string): LoginDTO => {
-  return {
-    full_name: fullName || "User",
-    refresh_token: sbResponse.session.refresh_token || "",
-    access_token: sbResponse.session.access_token || "",
-  };
-};
 
 type LoginEntity = {
   user: {
--- src/models/loginDTO.ts
+++
@@ -1,5 +0,0 @@
-export interface LoginDTO {
-  full_name: string;
-  access_token: string;
-  refresh_token: string;
-}
