f569a41b7747b0ce62c00be8f330ad3468b10910
angelgamonal <angelgamonal.t@gmail.com>
2025-08-08 15:50:26 -0500

    refactor: comment out authentication middleware in AI request errors route

--- src/routes/ai_request_errors.route.ts
+++ src/routes/ai_request_errors.route.ts
@@ -2,11 +2,11 @@ import { Router } from "express";
 import { validate } from "../middleware/validate";
 import { aiRequestErrorsSchema } from "../validators/ai_request_errors/ai_request_errors.validator";
 import { createAiRequestErrorsController } from "../controllers/aiRequestErrorsController";
-import { authenticate } from "../middleware/validateJwt";
+// import { authenticate } from "../middleware/validateJwt";
 
 const aiRequestErrorsRoutes = Router();
 
-aiRequestErrorsRoutes.use(authenticate);
+// aiRequestErrorsRoutes.use(authenticate);
 
 aiRequestErrorsRoutes.post(
   "/",
